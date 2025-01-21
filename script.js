8a616157fa60d849915c2491e087028b4b5deec5
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2025-01-20 21:41:34 -0800

    temporary update (cover screen)
    -added ability to press button to switch screens

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const coverScreen = document.querySelector('.cover');
+    CheckCover(coverScreen);
+});
+
+function CheckCover(coverScreen){
+    if (coverScreen) {
+        // Listen for keydown events
+        document.addEventListener('keydown', function (event) {
+            if (event.key === "ArrowRight") { // Right arrow key
+                coverScreen.setAttribute("data-covered", "create_covered");
+            } else if (event.key === "ArrowLeft") { // Left arrow key
+                coverScreen.setAttribute("data-covered", "login_covered");
+            }
+        });
+    } else {
+        console.error("Element with class 'cover' not found.");
+    }
+}
