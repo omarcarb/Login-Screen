3d40f9b0762e2e7b8d612e476bc034d3ba374c3c
Omar Carbajal <33193266+omarcarb@users.noreply.github.com>
2025-01-27 22:08:04 -0800

    added scattered circles

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let coverHeader = ["Good to See You Again!", "Be Part of Something Bigger."]
 let coverSubheading = ["Log in to reconnect and dive back into what you love.","Join to unlock exclusive features and connect with a community that gets you."]
 
+let circleScatter = '<svg width="52" height="53" viewBox="0 0 52 53" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="26" cy="26.5" rx="26" ry="26.5" fill="#12B0AC"/></svg>'
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const coverScreen = document.querySelector('.cover');
     const passwordRequirements = document.querySelector(".password_validation_container")
@@ -14,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     CheckCover(coverScreen, headerCover, subheaderCover);
     OpenPasswordContainer(passwordRequirements, passwordInput)
     PasswordCheck(passwordInput);
+    ScatterCircles(circleScatter, coverScreen);
 });
 
 function CheckCover(coverScreen, headerCover, subheaderCover){
@@ -104,4 +108,23 @@ function PasswordCheck(passwordInput){
             upperCaseCheck.setAttribute('data-status', 'invalid')
         }
     })
+}
+
+function ScatterCircles(circles, coverScreen){
+    let maxCircles = 0;
+
+    while(maxCircles < 20){
+        console.log(maxCircles)
+        const circleContainer = document.createElement('div');
+
+        circleContainer.innerHTML = circles;
+        circleContainer.setAttribute('id', 'circle-${maxCircles}')
+
+        circleContainer.style.position = 'absolute'
+        circleContainer.style.left = maxCircles * Math.floor((Math.random() * 10) + 1) + "%"
+        circleContainer.style.top = maxCircles * Math.floor((Math.random() * 10) + 1) + "%"
+        
+        coverScreen.appendChild(circleContainer);
+        maxCircles++;
+    }
 }
